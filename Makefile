c0261ff31ddb7eb6cefdd0a1d2f909ce12187a79
amr <abuelhassan93@gmail.com>
2025-02-14 22:10:03 +0000

    unique id challenge

--- Makefile
+++ Makefile
@@ -7,3 +7,8 @@ run-echo:
 	cd echo; go build -o $(EXECUTABLE_NAME)
 	cd -P $(MAELSTROM_PATH); ./maelstrom test -w echo --bin $(PROJECT_PATH)/echo/out --node-count 1 --time-limit 10
 	rm -f echo/$(EXECUTABLE_NAME)
+
+run-unique-id:
+	cd unique-id; go build -o $(EXECUTABLE_NAME)
+	cd -P $(MAELSTROM_PATH); ./maelstrom test -w unique-ids --bin $(PROJECT_PATH)/unique-id/out --time-limit 30 --rate 1000 --node-count 3 --availability total --nemesis partition
+	rm -f unique-id/$(EXECUTABLE_NAME)
---
+++ unique-id/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+
+	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main() {
+	n := maelstrom.NewNode()
+
+	n.Handle("generate", func(msg maelstrom.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+
+		body["type"] = "generate_ok"
+		return n.Reply(msg, map[string]any{
+			"type": "generate_ok",
+			"id":   time.Now().UnixNano() + int64(rand.Intn(100000)),
+		})
+	})
+
+	if err := n.Run(); err != nil {
+		log.Printf("err: %s", err)
+		os.Exit(1)
+	}
+}
