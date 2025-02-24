6169616c838f089546f0975a10fb238ab7e634f6
amr <abuelhassan93@gmail.com>
2025-02-24 21:50:37 +0000

    use stronger types

--- main.go
+++ main.go
@@ -25,7 +25,7 @@ const (
 )
 
 const (
-	counterSumKey = "SUM"
+	counterSumKey = "__SUM"
 
 	kafkaOffsetsKey   = "__OFFSETS"
 	kafkaCommittedKey = "__COMMITTED"
@@ -35,59 +35,63 @@ var AppType string
 
 type broadcast struct {
 	mu   sync.Mutex
-	vals []any
-	mp   map[float64]struct{}
+	vals []int
+	mp   map[int]struct{}
 }
 
 type server struct {
 	n            *maelstrom.Node
 	bcast        broadcast
-	bcastBatcher batcher.Batcher[any]
+	bcastBatcher batcher.Batcher[int]
 	cntrKV       *maelstrom.KV
 	kafkaKV      *maelstrom.KV
 }
 
 // Echo Challenge
-func (s *server) echoHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
+func (s *server) echoHandler(m maelstrom.Message) error {
+	var body map[string]any
+	readInto(m.Body, &body)
 	body["type"] = "echo_ok"
-	return s.n.Reply(msg, body)
+	return s.n.Reply(m, body)
 }
 
 // Unique ID Challenge
-func (s *server) generateHandler(msg maelstrom.Message) error {
-	return s.n.Reply(msg, map[string]any{
+func (s *server) generateHandler(m maelstrom.Message) error {
+	return s.n.Reply(m, map[string]any{
 		"type": "generate_ok",
 		"id":   uint64(rand.Int64() + time.Now().UTC().UnixNano()),
 	})
 }
 
 // Broadcast Challenge
-func (s *server) broadcastReadHandler(msg maelstrom.Message) error {
-	return s.n.Reply(msg, map[string]any{
+func (s *server) broadcastReadHandler(m maelstrom.Message) error {
+	return s.n.Reply(m, map[string]any{
 		"type":     "read_ok",
 		"messages": s.bcast.vals,
 	})
 }
 
-func (s *server) broadcastTopologyHandler(msg maelstrom.Message) error {
+func (s *server) broadcastTopologyHandler(m maelstrom.Message) error {
 	// ignoring and just using a flat tree topology.
-	return s.n.Reply(msg, map[string]any{"type": "topology_ok"})
+	return s.n.Reply(m, map[string]any{"type": "topology_ok"})
 }
 
-func (s *server) broadcastHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
+func (s *server) broadcastHandler(m maelstrom.Message) error {
+	var body struct {
+		Message  int   `json:"message"`
+		Messages []int `json:"messages"`
+	}
+	readInto(m.Body, &body)
 
-	var vals []any
-	if msgs, ok := body["messages"]; ok {
-		vals = msgs.([]any)
+	var vals []int
+	if body.Messages != nil {
+		vals = body.Messages
 	} else {
-		vals = []any{body["message"]}
+		vals = []int{body.Message}
 	}
 
 	s.bcast.mu.Lock()
-	for _, it := range vals {
-		val := it.(float64)
+	for _, val := range vals {
 		if _, ok := s.bcast.mp[val]; ok {
 			continue
 		}
@@ -97,10 +101,10 @@ func (s *server) broadcastHandler(msg maelstrom.Message) error {
 	}
 	s.bcast.mu.Unlock()
 
-	return s.n.Reply(msg, map[string]any{"type": "broadcast_ok"})
+	return s.n.Reply(m, map[string]any{"type": "broadcast_ok"})
 }
 
-func (s *server) broadcast(vals []any) {
+func (s *server) broadcast(vals []int) {
 	fn := func(dest string) func(ctx context.Context) error {
 		return func(ctx context.Context) error {
 			_, err := s.n.SyncRPC(ctx, dest, map[string]any{
@@ -124,37 +128,40 @@ func (s *server) broadcast(vals []any) {
 }
 
 // Grow-Only Counter Challenge
-func (s *server) counterAddHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
-	delta := int(body["delta"].(float64))
-
-	kvUpdate[int](s.cntrKV, counterSumKey, func() int { return 0 }, func(cur int) int { return cur + delta })
-
-	return s.n.Reply(msg, map[string]any{
+func (s *server) counterAddHandler(m maelstrom.Message) error {
+	var body struct {
+		Delta int `json:"delta"`
+	}
+	readInto(m.Body, &body)
+	kvUpdate[int](s.cntrKV, counterSumKey, func() int { return 0 }, func(cur int) int { return cur + body.Delta })
+	return s.n.Reply(m, map[string]any{
 		"type": "add_ok",
 	})
 }
 
-func (s *server) counterReadHandler(msg maelstrom.Message) error {
+func (s *server) counterReadHandler(m maelstrom.Message) error {
 	retryWrapped(func(ctx context.Context) error {
 		return s.cntrKV.Write(ctx, "rand", rand.Int())
 	})
 	sum, _, _ := kvRead[int](s.cntrKV, counterSumKey)
-	return s.n.Reply(msg, map[string]any{
+	return s.n.Reply(m, map[string]any{
 		"type":  "read_ok",
 		"value": sum,
 	})
 }
 
 // Kafka-Style Logs
-func (s *server) kafkaSendHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
-	key, val := body["key"].(string), int(body["msg"].(float64))
+func (s *server) kafkaSendHandler(m maelstrom.Message) error {
+	var body struct {
+		Key string `json:"key"`
+		Msg int    `json:"msg"`
+	}
+	readInto(m.Body, &body)
 
 	var offset int
 	kvUpdate[int](
 		s.kafkaKV,
-		fmt.Sprintf("%s_%s", kafkaOffsetsKey, key),
+		fmt.Sprintf("%s_%s", kafkaOffsetsKey, body.Key),
 		func() int { return -1 },
 		func(cur int) int {
 			offset = cur + 1
@@ -163,21 +170,22 @@ func (s *server) kafkaSendHandler(msg maelstrom.Message) error {
 	)
 
 	retryWrapped(func(ctx context.Context) error {
-		return s.kafkaKV.Write(ctx, fmt.Sprintf("%s_%d", key, offset), val)
+		return s.kafkaKV.Write(ctx, fmt.Sprintf("%s_%d", body.Key, offset), body.Msg)
 	})
-	return s.n.Reply(msg, map[string]any{
+	return s.n.Reply(m, map[string]any{
 		"type":   "send_ok",
 		"offset": offset,
 	})
 }
 
-func (s *server) kafkaPollHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
-	offsets := body["offsets"].(map[string]any)
+func (s *server) kafkaPollHandler(m maelstrom.Message) error {
+	var body struct {
+		Offsets map[string]int
+	}
+	readInto(m.Body, &body)
 
 	resp := map[string][][]int{}
-	for key, val := range offsets {
-		offset := int(val.(float64))
+	for key, offset := range body.Offsets {
 		for ; ; offset++ {
 			lg, ok, _ := kvRead[int](s.kafkaKV, fmt.Sprintf("%s_%d", key, offset))
 			if ok {
@@ -188,18 +196,20 @@ func (s *server) kafkaPollHandler(msg maelstrom.Message) error {
 		}
 	}
 
-	return s.n.Reply(msg, map[string]any{
+	return s.n.Reply(m, map[string]any{
 		"type": "poll_ok",
 		"msgs": resp,
 	})
 }
 
-func (s *server) kafkaCommitHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
-	offsets := body["offsets"].(map[string]any)
+func (s *server) kafkaCommitHandler(m maelstrom.Message) error {
+	var body struct {
+		Offsets map[string]int
+	}
+	readInto(m.Body, &body)
+	offsets := body.Offsets
 
-	for key, val := range offsets {
-		offset := int(val.(float64))
+	for key, offset := range offsets {
 		offsetKey := fmt.Sprintf("%s_%s", kafkaCommittedKey, key)
 		retryWrapped(func(ctx context.Context) error {
 			committed, _, _ := kvRead[int](s.kafkaKV, offsetKey)
@@ -209,22 +219,24 @@ func (s *server) kafkaCommitHandler(msg maelstrom.Message) error {
 			return s.kafkaKV.CompareAndSwap(ctx, offsetKey, committed, offset, true)
 		})
 	}
-	return s.n.Reply(msg, map[string]any{
+	return s.n.Reply(m, map[string]any{
 		"type": "commit_offsets_ok",
 	})
 }
 
-func (s *server) kafkaListCommitsHandler(msg maelstrom.Message) error {
-	body := readBody(msg)
-	keys := body["keys"].([]any)
+func (s *server) kafkaListCommitsHandler(m maelstrom.Message) error {
+	var body struct {
+		Keys []string `json:"keys"`
+	}
+	readInto(m.Body, &body)
 	resp := map[string]int{}
-	for _, key := range keys {
+	for _, key := range body.Keys {
 		val, ok, _ := kvRead[int](s.kafkaKV, fmt.Sprintf("%s_%s", kafkaCommittedKey, key))
 		if ok {
-			resp[key.(string)] = val
+			resp[key] = val
 		}
 	}
-	return s.n.Reply(msg, map[string]any{
+	return s.n.Reply(m, map[string]any{
 		"type":    "list_committed_offsets_ok",
 		"offsets": resp,
 	})
@@ -232,7 +244,7 @@ func (s *server) kafkaListCommitsHandler(msg maelstrom.Message) error {
 
 func main() {
 	const bcastBatchingLimit = 50
-	const bcastBatchingDuration = 450 * time.Millisecond
+	const bcastBatchingDuration = 500 * time.Millisecond
 
 	n := maelstrom.NewNode()
 	srv := server{n: n}
@@ -243,8 +255,8 @@ func main() {
 	case typeGenerate:
 		n.Handle("generate", srv.generateHandler)
 	case typeBroadcast:
-		srv.bcast = broadcast{vals: []any{}, mp: map[float64]struct{}{}}
-		srv.bcastBatcher = batcher.New[any](context.Background(), bcastBatchingLimit, bcastBatchingDuration, srv.broadcast)
+		srv.bcast = broadcast{vals: []int{}, mp: map[int]struct{}{}}
+		srv.bcastBatcher = batcher.New[int](context.Background(), bcastBatchingLimit, bcastBatchingDuration, srv.broadcast)
 		n.Handle("broadcast", srv.broadcastHandler)
 		n.Handle("topology", srv.broadcastTopologyHandler)
 		n.Handle("read", srv.broadcastReadHandler)
@@ -266,12 +278,10 @@ func main() {
 	}
 }
 
-func readBody(msg maelstrom.Message) map[string]any {
-	var body map[string]any
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		log.Panicf("Failed to unmarshal %s, err: %s", msg.Body, err.Error())
+func readInto(body json.RawMessage, val any) {
+	if err := json.Unmarshal(body, val); err != nil {
+		log.Panicf("Failed to unmarshal %s, err: %s", body, err.Error())
 	}
-	return body
 }
 
 func retryWrapped(fn func(context.Context) error) {
@@ -308,6 +318,7 @@ func kvRead[VT any](kv *maelstrom.KV, key string) (VT, bool, error) {
 	if err == nil {
 		return val, true, nil
 	}
+
 	var rpcErr *maelstrom.RPCError
 	if errors.As(err, &rpcErr) && rpcErr.Code == maelstrom.KeyDoesNotExist {
 		return val, false, nil
